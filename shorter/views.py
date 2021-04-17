7c00fee3836a1b455505f28e3215ac9dd4086629
sadra <sadrataghavi1383@gmail.com>
2021-04-17 09:21:02 +0430

    all functionalities all currently working ready to add features

--- shorter/views.py
+++ shorter/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Url
 from django.http import HttpResponse
+import uuid
 
 # Create your views here.
 
 def index(request):
     return render(request,"base.html")
+
+def create(request):
+    if request.method == "POST":
+        link = request.POST["link"]
+        uid = str(uuid.uuid4())[:5]
+        new_url = Url(link=link,uuid=uid)
+        new_url.save()
+        return HttpResponse(uid)
+def go(request,pk):
+    url_details = Url.objects.get(uuid=pk)
+    return redirect("https://"+url_details.link)
